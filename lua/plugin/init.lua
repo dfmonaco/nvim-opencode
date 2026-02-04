ce2e1a386600993821c98a78056b5bc46b7f6f7e
Diego Mónaco <dfmonaco@gmail.com>
2026-02-03 22:12:24 -0300

    feat: map <leader>O to toggle_terminal by default
    Add a simple default keymap in normal mode: <leader>O toggles the Opencode terminal. No <Plug> mapping or override checking.

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -9,10 +9,18 @@ function M.setup(opts)
   opts = opts or {}
   
   -- Register OC command
-  vim.api.nvim_create_user_command('OC', function()
+   vim.api.nvim_create_user_command('OC', function()
     require('plugin.commands').toggle_terminal()
   end, { desc = 'Open terminal in vertical split on the right' })
-  
+
+   -- Directly map <leader>O to toggle_terminal (normal mode)
+   vim.keymap.set('n', '<leader>O', function()
+     require('plugin.commands').toggle_terminal()
+   end, {
+     desc = 'Toggle Opencode terminal',
+     noremap = true,
+     silent = true,
+   })
 end
 
 return M
