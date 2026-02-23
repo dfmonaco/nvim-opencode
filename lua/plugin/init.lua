0a5511c5abbd0ab2863d7304deea488a3a3a3ec6
Diego Mónaco <dfmonaco@gmail.com>
2026-02-22 22:56:40 -0300

    feat(keymaps): add half page scroll, compact, and agent cycle bindings
    Add OCHalfPageDown (<Down>), OCHalfPageUp (<Up>), OCCompact (<leader>osc),
    and OCAgentCycle (<C-\>) keymaps with corresponding user commands.

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -43,6 +43,26 @@ function M.setup(opts)
     require('plugin.commands.tui').new_session()
   end, { desc = 'Start a new OpenCode session' })
 
+  -- Register OCHalfPageDown command
+  vim.api.nvim_create_user_command('OCHalfPageDown', function()
+    require('plugin.commands.tui').execute('session.half.page.down')
+  end, { desc = 'Scroll TUI half page down' })
+
+  -- Register OCHalfPageUp command
+  vim.api.nvim_create_user_command('OCHalfPageUp', function()
+    require('plugin.commands.tui').execute('session.half.page.up')
+  end, { desc = 'Scroll TUI half page up' })
+
+  -- Register OCCompact command
+  vim.api.nvim_create_user_command('OCCompact', function()
+    require('plugin.commands.tui').execute('session.compact')
+  end, { desc = 'Compact the current OpenCode session' })
+
+  -- Register OCAgentCycle command
+  vim.api.nvim_create_user_command('OCAgentCycle', function()
+    require('plugin.commands.tui').execute('agent.cycle')
+  end, { desc = 'Cycle to the next OpenCode agent' })
+
   require('plugin.keymaps').setup(opts)
   require('plugin.autocmds').setup(opts)
 end
--- lua/plugin/keymaps.lua
+++ lua/plugin/keymaps.lua
@@ -6,50 +6,78 @@ function M.setup(opts)
   opts = opts or {}
 
   -- Toggle terminal
-  vim.keymap.set('n', '<leader>O', '<cmd>OC<cr>', {
+  vim.keymap.set('n', '<leader>oo', '<cmd>OC<cr>', {
     desc = 'Toggle terminal (vertical right split)',
     noremap = true,
     silent = true,
   })
 
   -- Open/focus prompt buffer
-  vim.keymap.set('n', '<leader>Op', '<cmd>OCPrompt<cr>', {
+  vim.keymap.set('n', '<C-Space>', '<cmd>OCPrompt<cr>', {
     desc = 'Open/focus OCPrompt buffer in this window',
     noremap = true,
     silent = true,
   })
 
   -- Send buffer or selection to session
-  vim.keymap.set({ 'n', 'v' }, '<leader>Os', '<cmd>OCSend<cr>', {
+  vim.keymap.set({ 'n', 'v' }, '<leader>om', '<cmd>OCSend<cr>', {
     desc = 'Send current buffer (or visual selection) to OpenCode session',
     noremap = true,
     silent = true,
   })
 
   -- Append buffer or selection to TUI prompt
-  vim.keymap.set({ 'n', 'v' }, '<leader>Oa', '<cmd>OCTuiAppend<cr>', {
+  vim.keymap.set({ 'n', 'v' }, '<leader>opa', '<cmd>OCTuiAppend<cr>', {
     desc = 'Append current buffer or visual selection to the TUI prompt',
     noremap = true,
     silent = true,
   })
 
   -- Append buffer or selection to TUI prompt and submit
-  vim.keymap.set({ 'n', 'v' }, '<leader>OS', '<cmd>OCTuiSend<cr>', {
+  vim.keymap.set({ 'n', 'v' }, '<leader>opm', '<cmd>OCTuiSend<cr>', {
     desc = 'Append current buffer or visual selection to TUI prompt and submit',
     noremap = true,
     silent = true,
   })
 
   -- Interrupt the current AI run
-  vim.keymap.set('n', '<leader>Oi', '<cmd>OCInterrupt<cr>', {
+  vim.keymap.set('n', '<leader>osi', '<cmd>OCInterrupt<cr>', {
     desc = 'Interrupt the current OpenCode AI run',
     noremap = true,
     silent = true,
   })
 
   -- Start a new session
-  vim.keymap.set('n', '<leader>On', '<cmd>OCNewSession<cr>', {
+  vim.keymap.set('n', '<leader>osn', '<cmd>OCNewSession<cr>', {
     desc = 'Start a new OpenCode session',
+    noremap = true,
+    silent = true,
+  })
+
+  -- Scroll TUI half page down
+  vim.keymap.set('n', '<Down>', '<cmd>OCHalfPageDown<cr>', {
+    desc = 'Scroll OpenCode TUI half page down',
+    noremap = true,
+    silent = true,
+  })
+
+  -- Scroll TUI half page up
+  vim.keymap.set('n', '<Up>', '<cmd>OCHalfPageUp<cr>', {
+    desc = 'Scroll OpenCode TUI half page up',
+    noremap = true,
+    silent = true,
+  })
+
+  -- Compact the current session
+  vim.keymap.set('n', '<leader>osc', '<cmd>OCCompact<cr>', {
+    desc = 'Compact the current OpenCode session',
+    noremap = true,
+    silent = true,
+  })
+
+  -- Cycle to the next agent
+  vim.keymap.set('n', '<C-\\>', '<cmd>OCAgentCycle<cr>', {
+    desc = 'Cycle to the next OpenCode agent',
     noremap = true,
     silent = true,
   })
